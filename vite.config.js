8081ab59937f8b2caed8af918bb42bf9f909449e
Nikhil pandey <121350935+Nikhilpandey8@users.noreply.github.com>
2025-07-08 12:18:23 +0530

    Add vite.config.js 1751914639594

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['4zq77p-8000.csb.app']
+  }
+});
